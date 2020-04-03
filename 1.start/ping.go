ae23a1401a6d4c981ffd90bd88cac4cb4ae99964
小林 刘 <xiaolin-liu@qq.com>
2020-04-03 14:18:59 +0800

    gin start

---
+++ 1.start/ping.go
@@ -0,0 +1,14 @@
+package main
+
+func setupRouter() *gin.Engine {
+    r := gin.Default()
+    r.GET("/ping", func(c *gin.Context) {
+        c.String(200, "pong")
+    })
+    return r
+}
+
+func main() {
+    r := setupRouter()
+    r.Run(":8080")
+}
