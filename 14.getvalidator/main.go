361cf03af457c557213f7faa8797964fb81568a9
Liu-Alan <xiaolin-liu@qq.com>
2020-04-17 22:58:03 +0800

    validator

---
+++ 14.getvalidator/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Person struct {
+	Name    string `form:"name"`
+	Address string `form:"address"`
+}
+
+func main() {
+	router := gin.Default()
+
+	router.Any("/testing", startPage)
+	router.Run(":8080")
+}
+
+func startPage(c *gin.Context) {
+	var person Person
+	if c.ShouldBindQuery(&person) == nil {
+		log.Println("====== Only Bind By Query String ======")
+		log.Println(person.Name)
+		log.Println(person.Address)
+	}
+
+	c.String(200, "Success")
+}
