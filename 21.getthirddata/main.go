0541104f014a45c6d860cc6981b5550dadc80308
Liu-Alan <xiaolin-liu@qq.com>
2020-05-02 12:10:52 +0800

    get third data

--- 21.getthirddata/main.go
+++ 21.getthirddata/main.go
@@ -13,6 +13,18 @@ func main() {
 		response, err := http.Get("http://snaill.net/sitedata/image/nginx_1_2.png")
 		if err != nil || response.StatusCode != http.StatusOK {
 			c.Status(http.StatusServiceUnavailable)
+			return
 		}
+
+		reader := response.Body
+		contentLength := response.ContentLength
+		contentType := response.Header.Get("Content-Type")
+
+		extraHeaders := map[string]string{
+			"Content-Disposition": `attachment;filename="nginx_1_2.png"`,
+		}
+		c.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
 	})
+
+	router.Run(":8080")
 }
